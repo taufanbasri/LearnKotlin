6360e615d30aee8fbe9d6a87cf742025d4f5a864
taufanbasri <taufanprasetyobasri@gmail.com>
2020-02-18 00:31:11 +0700

    Data Types in Kotlin

--- src/HelloWorld.kt
+++ src/HelloWorld.kt
@@ -1,4 +1,22 @@
 fun main(args:Array<String>){
-    println("Hello World")
-    print("Welcome to Kotlin")
+    var email:String?
+    email = "taufanprasetyobasri@gmail.com"
+
+    print(email)
+
+    // Int
+    var a: Int = 20
+    var b: Int = -10
+
+    // String
+    var name: String = "Taufan Prasetyo"
+
+    // Float
+    var sum: Float = 20.5F
+
+    // Char
+    var option: Char = 'a'
+
+    // Boolean
+    var test: Boolean = true
 }
